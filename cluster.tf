a10862a05335e752142a5914a332ee9b94bdfe00
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-23 23:12:56 +0100

    cluster.tf[update]

--- cluster.tf
+++ cluster.tf
@@ -1,11 +1,11 @@
 resource "kind_cluster" "default" {
     name = "${var.cluster_name}--${var.branch}"
     node_image = var.node_image
-    kind_config_yaml = <<YAML
-    kind: Cluster
-    apiVersion: kind.x-k8s.io/v1alpha4
-    nodes:
-    - role: control-plane
-    - role: worker
-    YAML
+    wait_for_ready = true
+    kind_config {
+        kind        = "Cluster"
+        api_version = "kind.x-k8s.io/v1alpha4"
+
+        
+    }
 }
