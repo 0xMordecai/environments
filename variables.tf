45ca323ed114977f10fdab52032fa87420fdd568
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-23 20:23:57 +0100

    variables.tf[Update]

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "branch" {
+  description = "branch"
+  type        = string
+  default     = "dev" 
+}
+
+variable "cluster_name" {
+  description = "cluster name"
+  type        = string
+  default     = "project-cluster"
+}
+
+variable "node_image" {
+  description = "node image"
+  type = string
+  default = "kindest/node:v1.34.3"
+}
