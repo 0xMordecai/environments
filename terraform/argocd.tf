ddeda4d2fb602a38d61b34a9f9fc4f251b47ac19
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-27 22:24:18 +0100

    terraform/argocd.tf[create two kubectl_manifest resources for Argo CD app]

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -14,3 +14,11 @@ resource "kubectl_manifest" "namespace" {
     yaml_body   = each.value
     override_namespace  = "argocd"
 }
+resource "kubectl_manifest" "argocd" {
+    depends_on = [ 
+        kubectl_manifest.namespace,
+    ]
+    for_each = data.kubectl_file_documents.argocd.manifests
+    yaml_body = each.value
+    override_namespace = "argocd"
+}
