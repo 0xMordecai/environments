684c3efcb00d2c53db7ac3649b33348e4bac2e72
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-12-27 22:14:02 +0100

    terraform/argocd.tf[create kubectl_file_documents data sources for argo CD app ]

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -1,3 +1,6 @@
 data "kubectl_file_document" "namespace" {
     content = file("../manifests/argo-cd/namespace.yaml")
 }
+data "kubectl_file_document" "argocd" {
+    content = file("../manifests/argo-cd/install.yaml")
+}
