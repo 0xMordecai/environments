7cf3f1b8a45f84b4ab02dfee5c1f9d9e4475a8f7
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-01-03 22:18:11 +0100

    terraform/cluster.tf[ update ]

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -1,6 +1,6 @@
 resource "kind_cluster" "default" {
     name = "${var.cluster_name}--${var.branch}"
-    node_image = var.node_image
+    # node_image = var.node_image
     wait_for_ready = true
     kind_config {
         kind        = "Cluster"
