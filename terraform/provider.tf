04bcb8effd8857c65b54cab6da2c0b5187b3b277
0xMordecai <sir.lahrach.mohamed@gmail.com>
2026-01-03 21:11:40 +0100

    terraform/provider.tf[ update ]

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,12 +4,8 @@ terraform {
       source = "ryanfaircloth/kind"
       version = "2.0.0"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
-    }
   }
 }
 
 provider "kind" {}
-provider "kubectl" {}
+# provider "kubectl" {}
